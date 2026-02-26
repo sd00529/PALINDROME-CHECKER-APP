8db77d4a3db0f6ef5d87d29e034d2112f8e585b0
Simran Das <mailme.simrandas@gmail.com>
2026-02-26 23:06:10 +0530

    Initial commit - Palindrome Checker App

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+/**
+ * Use Case 1: Application Entry & Welcome Message
+ * Entry point of the Palindrome Checker application.
+ * Version: 1.0
+ */
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
