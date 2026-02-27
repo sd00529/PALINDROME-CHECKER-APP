6182f7b9aad651933a9e8220b33921e20730c574
Simran Das <mailme.simrandas@gmail.com>
2026-02-27 09:58:23 +0530

    UC13 Added Palindrome Checker App

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,33 +3,53 @@ import java.util.*;
 public class PalindromeCheckerApp{
 
     public static void main(String[] args) {
-//UC12
+//UC13
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Input: ");
+        System.out.print("Input : ");
         String input = scanner.nextLine();
 
-        // Inject strategy at runtime
-        PalindromeStrategy strategy = new StackStrategy();
+        // ---------- Two Pointer Benchmark ----------
+        long start1 = System.nanoTime();
+        boolean result1 = twoPointerCheck(input);
+        long end1 = System.nanoTime();
 
-        boolean result = strategy.check(input);
+        // ---------- Stack Benchmark ----------
+        long start2 = System.nanoTime();
+        boolean result2 = stackCheck(input);
+        long end2 = System.nanoTime();
 
-        System.out.println("Is Palindrome?: " + result);
+        System.out.println("\nTwo Pointer Result : " + result1);
+        System.out.println("Execution Time : " + (end1 - start1) + " ns");
+
+        System.out.println("\nStack Result : " + result2);
+        System.out.println("Execution Time : " + (end2 - start2) + " ns");
 
         scanner.close();
     }
+    private static boolean twoPointerCheck(String input) {
 
-}
-interface PalindromeStrategy {
+        int left = 0;
+        int right = input.length() - 1;
 
-    boolean check(String input);
-}
-class StackStrategy implements PalindromeStrategy {
-    public boolean check(String input) {
-        java.util.Stack<Character> stack = new java.util.Stack<>();
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+    private static boolean stackCheck(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
         for (char c : input.toCharArray()) {
             stack.push(c);
         }
+
         for (char c : input.toCharArray()) {
             if (c != stack.pop()) {
                 return false;
@@ -37,6 +57,6 @@ class StackStrategy implements PalindromeStrategy {
         }
 
         return true;
-
     }
+
 }
