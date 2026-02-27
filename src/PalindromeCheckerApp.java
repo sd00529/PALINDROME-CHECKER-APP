900deb154eda613fda888deee4859b2b8e55e026
Simran Das <mailme.simrandas@gmail.com>
2026-02-27 08:45:10 +0530

    UC5 Added Palindrome Checker App

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,23 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp{
     public static void main(String[] args) {
-//UC4
-        String input = "radar";
+//UC5
+        String input = "noon";
 
-        char[] chars = input.toCharArray();
+        Stack<Character> stack = new Stack<>();
 
-        int start = 0;
-        int end = chars.length - 1;
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
 
         boolean isPalindrome = true;
 
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
 
         System.out.println("Input : " + input);
